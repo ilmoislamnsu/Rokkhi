9786f39e56395a261f10479fada8ff051cf3229c
Md Shafiqul Islam <ilmoislam@icloud.com>
2019-11-14 22:09:27 +0600

    #4 Date Picker Done

--- rokkhiOffice/Controller/InviteVC.swift
+++ rokkhiOffice/Controller/InviteVC.swift
@@ -9,31 +9,72 @@
 import UIKit
 
 class InviteVC: UIViewController {
-
+    
     @IBOutlet weak var addFromContactBtn: UIButton!
     @IBOutlet weak var invitePicture: UIImageView!
+    @IBOutlet weak var dateDisplay: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createToolbar()  //Toolbar Date Picker
+        
         invitePicture.layer.masksToBounds = true
         invitePicture.layer.cornerRadius = invitePicture.layer.frame.width / 2
         addFromContactBtn.layer.masksToBounds = true
-        //addFromContactBtn.layer.cornerRadius = 25
         addFromContactBtn.layer.cornerRadius = addFromContactBtn.layer.frame.height / 2
-        
         addFromContactBtn.layer.borderColor = #colorLiteral(red: 0.3607843137, green: 0.8039215686, blue: 0.1294117647, alpha: 1)
         addFromContactBtn.layer.borderWidth = 1.5
-        // Do any additional setup after loading the view.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //Step 1: Create An Outlet of text Field Naming "dateDisplay"
+    //Step 2: Under the superViewDidload add CreateToolbar()
+    let picker = UIDatePicker() //For Adding Date Picker with done button
+    //Toolbar Date Picker
+    func createToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(self.showDate))
+        toolbar.setItems([done] , animated: false)
+        dateDisplay.inputAccessoryView = toolbar
+        dateDisplay.inputView = picker
+        picker.datePickerMode = .date
+        
     }
-    */
-
+    @objc func showDate(){
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "dd-MM-YYYY"
+        let string = dateFormat.string(from: picker.date)
+        dateDisplay.text = string
+        self.view.endEditing(true)
+    }
+    //End Of Toolbar Date Picker
+    //
+    
+    
+    
+    
 }
